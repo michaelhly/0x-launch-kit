c6927beea03cf1163b33139e856b70f0ca22a18f
michaelhly <michaelhly@gmail.com>
2019-02-03 19:40:35 -0600

    removed buying of complete sets

--- ts/src/contract_calls.ts
+++ ts/src/contract_calls.ts
@@ -2,15 +2,7 @@ import { BigNumber, ContractWrappers, Provider, SignedOrder } from '0x.js';
 import { Web3Wrapper } from '@0x/web3-wrapper';
 import { TransactionReceiptWithDecodedLogs } from 'ethereum-types';
 
-import {
-    FEE_RECIPIENT,
-    FEE_RECIPIENT_PRIVATE_KEY,
-    INFURA_API_KEY,
-    NETWORK_ID,
-    RINKEBY_VEIL_CS,
-    RPC_URL,
-} from './config';
-import { RADIX_STRING, ZRX_DECIMALS } from './constants';
+import { FEE_RECIPIENT, FEE_RECIPIENT_PRIVATE_KEY, INFURA_API_KEY, NETWORK_ID, RPC_URL } from './config';
 
 // tslint:disable no-var-requires
 const HDWalletProvider = require('truffle-hdwallet-provider');
@@ -19,9 +11,6 @@ const provider = new HDWalletProvider(FEE_RECIPIENT_PRIVATE_KEY, `${RPC_URL}/${I
 const Web3 = require('web3');
 const web3 = new Web3(provider);
 const currentProvider: Provider = web3.currentProvider;
-
-const AugurAddresses = require('augur-core-abi').addresses[NETWORK_ID];
-const VeilCompleteSets = require('veil-contracts/build/contracts/VeilCompleteSets.json');
 
 const get_contractWrappers = (): ContractWrappers => {
     return new ContractWrappers(currentProvider, { networkId: NETWORK_ID });
@@ -32,38 +21,6 @@ const get_web3Wrapper = (contractWrapper: ContractWrappers): Web3Wrapper => {
     web3Wrapper.abiDecoder.addABI(contractWrapper.exchange.abi);
     web3Wrapper.abiDecoder.addABI(contractWrapper.erc20Token.abi);
     return web3Wrapper;
-};
-
-export const buy_complete_sets = async (
-    market: string,
-    longToken: string,
-    shortToken: string,
-    shareAmount: BigNumber,
-    numTicks: number,
-): Promise<any | null> => {
-    const veilCS = new web3.eth.Contract(VeilCompleteSets.abi, RINKEBY_VEIL_CS);
-    try {
-        const amount = Web3Wrapper.toBaseUnitAmount(shareAmount, ZRX_DECIMALS);
-        const tx = await veilCS.methods
-            .buyCompleteSets(
-                AugurAddresses.Augur,
-                AugurAddresses.Cash,
-                AugurAddresses.CompleteSets,
-                market,
-                longToken,
-                shortToken,
-                amount,
-            )
-            .send({
-                from: web3.currentProvider.addresses[0],
-                value: parseInt(amount.toString(), RADIX_STRING) * numTicks,
-            });
-        return tx;
-    } catch (err) {
-        // tslint:disable no-console
-        console.log(err);
-    }
-    return null;
 };
 
 export const batch_fill_or_kill = async (
